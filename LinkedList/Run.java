3848176f8f8f8ab2d135ce95ae4a55dcb9b41bae
Debashis Kar Suvra <54812722+Suvrakar@users.noreply.github.com>
2020-05-28 01:04:46 +0600

    Create Run.java

---
+++ LinkedList/Run.java
@@ -0,0 +1,16 @@
+package linkedList;
+
+public class Run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList list=new LinkedList();
+
+		list.insert(4);
+		list.insert(7);
+		list.insert(9);
+		
+		list.print();
+	}
+
+}
